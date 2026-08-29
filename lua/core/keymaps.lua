798f2c17290af10f57c4b04dab67e30ab9316f93
TheCreeper5020 <TheCreeper5020@gmail.com>
2026-08-29 13:16:06 -0500

    Add colorscheme

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,7 +6,7 @@ vim.g.maplocalleader = " "
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
 
-vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlight", silent = true })
-vim.keymap.set("n", "<C-l>", ":echo<CR>", { desc = "Clear last executed command", silent = true })
+vim.keymap.set("n", "<C-c>", "<CMD>nohl<CR>", { desc = "Clear search highlight", silent = true })
+vim.keymap.set("n", "<C-l>", "<CMD>echo<CR>", { desc = "Clear last executed command", silent = true })
 
-vim.keymap.set("n", "-", ":Oil<CR>", opts)
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", opts)
